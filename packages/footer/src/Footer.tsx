42f183a7e5cbb31dd393e2be36e123934c3e9bfa
Victor Savkin <mail@vsavkin.com>
2022-06-03 14:47:20 -0400

    everything is set up

---
+++ packages/footer/src/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Footer() {
+    return (
+        <footer>
+            Footer
+        </footer>
+    );
+}
---
+++ packages/header/src/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Header() {
+    return (
+        <header>
+            Lerna is the original monorepo tool!
+        </header>
+    );
+}
