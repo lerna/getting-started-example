f3119014831a89deb5fb7bd322d87bfd055a89e8
Victor Savkin <mail@vsavkin.com>
2022-06-08 12:28:40 -0400

    wip

---
+++ clear.sh
@@ -0,0 +1,7 @@
+rm -rf packages/footer/dist
+rm -rf packages/footer/node_modules
+rm -rf packages/header/dist
+rm -rf packages/header/node_modules
+rm -rf packages/remixapp/build
+rm -rf packages/remixapp/cache
+rm -rf packages/remixapp/node_modules
