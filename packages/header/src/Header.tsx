04466438ab89ca717f1d8f5e3a5aa896fcfac9ba
tpimpinela <tom@spimpinela.com>
2023-11-21 22:24:21 +0100

    fix(header): put header correct color

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Header() {
   return (
-    <header style="color: red;">
+    <header style="color: blue;">
       Lerna is the original monorepo tool! HEH
     </header>
   );
