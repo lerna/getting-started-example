50aca9888c8a3042a76a3cb862995e332df28551
tpimpinela <tom@spimpinela.com>
2023-11-21 22:16:40 +0100

    feat(footer): add new text

--- packages/footer/src/Footer.tsx
+++ packages/footer/src/Footer.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
+import React from "react";
 
 export function Footer() {
-    return (
-        <footer>
-            Footer
-        </footer>
-    );
+  return <footer>Footer changed</footer>;
 }
